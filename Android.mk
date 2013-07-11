62a520009385ff4822c2d41f3b27f22a98f56e0d
David Keppler <dkeppler@mitre.org>
2013-07-11 16:02:35 -0400

    Stripped out unnecessary files
    Removed
    * protobuf sources
    * all of the old SVMP Protocol classes
    
    Left
    * protobuf-2.5.0 jar
    * generated SVMPProtocol class
    
    Next step, autogenerating from just the svmp.proto definition.
    
    Change-Id: I60e782c39bc015ff4ab14c22d3bcd5429191442a

--- Android.mk
+++ Android.mk
@@ -5,9 +5,14 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_STATIC_JAVA_LIBRARIES := protobuf-2.5.0
 LOCAL_MODULE := SVMPProtocol
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := protobuf-2.5.0:protobuf-2.5.0/protobuf-java-2.5.0.jar
+include $(BUILD_MULTI_PREBUILT)
 
 # Include this library in the build server's output directory
 $(call dist-for-goals, dist_files, $(LOCAL_BUILT_MODULE):SVMPProtocol.jar)
